52309a1a81e44e76796c14bd98136b9b8ce0c16f
qa-bwlvh-4-iarj@example.com <qa-bwlvh-4-iarj@example.com>
2023-02-08 18:26:02 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15191.stoplight-dev.com/api/v1/projects/cHJqOjI0Nw/spectral.js?branch_slug=main&token=3fbb503d-10b9-46f4-8e2c-f500bbc81756";
+export default { extends: ruleset };
